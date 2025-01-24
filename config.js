753bf74b71855b2c680c0308f768370c4050c5ea
0xBaiwan <1827143939@qq.com>
2025-01-24 16:13:41 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// 代理配置
+export default {
+    // 代理地址，格式：protocol://user:pass@host:port
+    // 示例：http://user123:pass456@proxy.example.com:8080
+    proxyUrl: "http://user:pass@proxy.example.com:8080", // 留空表示不使用代理
+};
